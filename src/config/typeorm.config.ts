c270b76301cf81bef74ebcb432a983129152f83e
Julián Prieto <herbie.juls@gmail.com>
2020-09-07 21:04:38 -0300

    Add Auth

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,22 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'lallah.db.elephantsql.com',
   port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'taskmanagement',
+  username: 'gtrapemj',
+  password: 'v6GNwXe0TJlfQ6tH70R9TcWd3CtFU7Xa',
+  database: 'gtrapemj',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'root',
+//   database: 'taskmanagement',
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   synchronize: true,
+// };
